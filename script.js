3d5a52878162a8ddf48146e354fdf3f9346e833f
maham-ishtiaq-2000 <mahamishtiaq2000@gmail.com>
2023-05-27 18:26:04 -0700

    myfirstcommit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["WebDeveloper"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
